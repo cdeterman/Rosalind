8ebe8d89a224e15430e08dfe2d36a52c243346da
cdeterman <cdetermanjr@hotmail.com>
2014-07-17 09:20:57 -0500

    Removed Identifiers

--- 009_SUBS.py
+++ 009_SUBS.py
@@ -12,7 +12,7 @@ from Bio import motifs
 from Bio.Seq import Seq
 import Bio.Alphabet
 
-f = open("C:/Users/Chaz/python_code/Rosalind/data/rosalind_subs.txt", 'r')
+f = open("data/rosalind_subs.txt", 'r')
 raw_seq = f.readline().rstrip()
 motif = f.readline().rstrip()
 f.close()
@@ -28,7 +28,7 @@ locations = []
 for pos in m.instances.search(seq):
     locations.append(pos[0]+1) # changes default from \n
 
-o = open("C:/Users/Chaz/python_code/Rosalind/output/009_SUBS.txt", 'w')
+o = open("output/009_SUBS.txt", 'w')
 o.write(" ".join(map(str, locations)))
 o.close()
 
