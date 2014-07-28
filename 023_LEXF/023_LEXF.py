e19a28f9edf1299d059e9e6b6128cdcac64f3093
Charles Determan <cdetermanjr@hotmail.com>
2014-07-28 12:21:45 -0500

    Genome Assembly Solution

---
+++ 023_LEXF/023_LEXF.py
@@ -0,0 +1,27 @@
+'''
+My solution to Rosalind Bioinformatics Problem 023
+
+Title: Enumerating k-mers Lexicographically
+Rosalind ID: LEXF
+Rosalind #: 023
+URL: http://rosalind.info/problems/lexf
+
+Goal to return all resampled permutations of a provided sequence in the
+lexicographical order of the sequence.
+'''
+
+from itertools import product
+
+f = open("data/rosalind_lexf.txt", 'r')
+alphabet = list(f.readline().rstrip().split())
+n = int(f.readline().rstrip())
+f.close()
+
+''' Used product instead of permutations to get resampling.
+    Also sorting is unneccessary because the inner product
+    produces the order of the sequence provided
+'''
+outhandle = open("output/024_LEXF.txt", 'w')
+for perm in product(alphabet, repeat=n): #note, repeat must be specifically stated
+    print >> outhandle, "".join(perm)
+outhandle.close()
---
+++ 025_PROB/025_PROB.py
@@ -0,0 +1,34 @@
+'''
+My solution to Rosalind Bioinformatics Problem 025
+
+Title: Introduction to Random Strings
+Rosalind ID: PROB
+Rosalind #: 025
+URL: http://rosalind.info/problems/prob
+
+Goal to return probability that random sequence will exactly match the
+provided sequence given the GC content.
+'''
+
+from math import log10
+
+f = open("data/rosalind_prob.txt")
+my_seq = f.readline().rstrip()
+gc_array = map(float, f.readline().rstrip().split())
+f.close()
+
+output_handle = open("output/025_PROB.txt", 'w')
+for gc in gc_array:
+    prob_gc = gc/2
+    prob_at = (1-gc)/2
+    #print prob_gc
+
+    prob = 1
+    for nucleotide in my_seq:
+        if nucleotide == 'A' or nucleotide == 'T':
+            prob *= prob_at
+        else:
+            prob *= prob_gc
+    print >> output_handle, log10(prob),
+
+output_handle.close()
