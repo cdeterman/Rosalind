39b73e96fd47babd5fed11500ebfa3cc567e6415
Charles Determan <cdetermanjr@hotmail.com>
2014-07-29 13:47:08 -0500

    PMCH Solution

---
+++ 027_PMCH/027_PMCH.py
@@ -0,0 +1,40 @@
+'''
+My solution to Rosalind Bioinformatics Problem 027
+
+Title: Perfect Matchings and RNA Secondary Structures
+Rosalind ID: PMCH
+Rosalind #: 027
+URL: http://rosalind.info/problems/pmch
+
+Goal to number of possible perfect matchings of basepairs in a sequence.
+Assumed to have the same number of A to U and C to G.
+Given the recurrence relationship described in the problem, this can
+be solved relatively easily withe factorials.  However, graph theory
+has other methods for counting the number of perfect matches involving
+adjacency matricies.  Something to potentially add on to this solution
+for more general application.
+'''
+
+from collections import Counter
+from math import factorial
+from Bio import SeqIO
+
+f = open("data/rosalind_pmch.txt", 'r')
+raw_data = SeqIO.read(f, "fasta")
+f.close()
+seq = str(raw_data.seq)
+
+counts = Counter(seq)
+
+''' This is your recursive calculation
+    You only use one from each pair of nucleotides otherwise it is
+    just duplications.  Then, the idea is the same thing as a normal
+    graph of (2n-1)*(2n-3)(2n-5)...
+    The factorial of A (e.g. 3*2*1) times factorial of C (e.g. 2*1) = 12
+'''
+pn = factorial(counts["A"])*factorial(counts["C"])
+
+
+o = open("output/027_PMCH.txt", 'w')
+print >> o, pn
+o.close()
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 These are my current solutions to the Rosalind Problems.  I am very fond of the Biopython toolset and try to apply it where appropriate.  As such, implementation of many of these solutions require its' installation.  I have tried to annotate my code and notify where installations may be necessary.  I generally work with Python 2.7 however I will work towards Python 3.X functionality.  I would like to create solutions with other programming languages but primarily these will consist of only python solutions.
 
 # Bioinformatics Stronghold
-# Currently Solved: 26 
+# Currently Solved: 27 
 * __Counting DNA Nucleotides__
 	* Language: Python, R
 * __Transcribing DNA into RNA__
@@ -55,6 +55,8 @@ These are my current solutions to the Rosalind Problems.  I am very fond of the 
 * __Longest Increasing Subsequence__
 	* Language: Python
 * __Genome Assembly as Longest Superstring__
+	* Language: Python
+* __Perfect Matchings and RNA Secondary Structures__
 	* Language: Python
 
 # Algorithmic Heights
